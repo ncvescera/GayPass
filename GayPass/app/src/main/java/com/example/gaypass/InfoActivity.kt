30536b497355f19131c63965e7501ace1ae34a7f
Nicolò Vescera <nicolo.vescera@gmail.com>
2021-08-24 19:39:41 +0200

    Themes
    Added themes on the Info Activity.
    Changed varsion number.
    ( #10  )

--- GayPass/app/src/main/java/com/example/gaypass/InfoActivity.kt
+++ GayPass/app/src/main/java/com/example/gaypass/InfoActivity.kt
@@ -3,6 +3,7 @@ package com.example.gaypass
 import android.os.Bundle
 
 import androidx.appcompat.app.AppCompatActivity
+import com.example.gaypass.Managers.ThemeManager
 
 class InfoActivity : AppCompatActivity() {
 
@@ -10,5 +11,7 @@ class InfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
 
+        // apply theme
+        ThemeManager(this, window, supportActionBar, findViewById(R.id.info_box)).applyTheme()
     }
 }
