c3065ebbaf74eb6f6ac36d8d65f01ae19edc1c0f
Nicolò Vescera <nicolo.vescera@gmail.com>
2021-09-01 10:59:14 +0200

    InfoActivity Description
    Added a better description on the InfoActivity.
    Added "Made By" section with links.
    Added GitHub Repo link

--- GayPass/app/src/main/java/com/example/gaypass/InfoActivity.kt
+++ GayPass/app/src/main/java/com/example/gaypass/InfoActivity.kt
@@ -1,6 +1,8 @@
 package com.example.gaypass
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gaypass.managers.ThemeManager
@@ -10,6 +12,9 @@ class InfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+
+        val textView = findViewById<TextView>(R.id.warningText)
+        textView.movementMethod = LinkMovementMethod.getInstance()
 
         // apply theme
         ThemeManager(this, window, supportActionBar, findViewById(R.id.info_box)).applyTheme()
